ecd55a6debc36d1b103f469ef59d609259e949d6
jero.hamalainen <jero.hamalainen@PC30453>
2021-10-08 10:23:35 +0300

    Add project files.

---
+++ Trigometry Dash/includes.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
+#include <cstdlib>
---
+++ Trigometry Dash/main.cpp
@@ -0,0 +1,10 @@
+#include "includes.h"
+
+int main() {
+	sf::RenderWindow MainWindow(sf::VideoMode(800, 800, 32), "Trigometry Dash");
+	bool gameloop = true;
+	while (gameloop) {
+
+	}
+	return 0;
+}
