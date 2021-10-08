9e6b964a46d2075c46e359da37668fde25646336
alex puustinen <greatmax25@yahoo.com>
2021-10-08 11:21:42 +0300

    8=D

--- Trigometry Dash/main.cpp
+++ Trigometry Dash/main.cpp
@@ -4,7 +4,7 @@ int main() {
 	sf::RenderWindow MainWindow(sf::VideoMode(800, 800, 32), "Trigometry Dash");
 	bool gameloop = true;
 	while (gameloop) {
-		std::cout << "lol";
+		std::cout << "8===D";
 	}
 	return 0;
 }
