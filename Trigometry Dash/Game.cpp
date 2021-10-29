814e14f4f425f919c7cc0aca7ec404b6cf102110
xzarfs <jero.hamalainen@gmail.com>
2021-10-29 11:56:03 +0300

    lol

--- Trigometry Dash/Game.cpp
+++ Trigometry Dash/Game.cpp
@@ -7,12 +7,12 @@ Game::Game(sf::Vector2i windowSize) {
 	background.setFillColor(sf::Color::Black);
 	background.setSize((sf::Vector2f)windowSize);
 
-	textureBackground_file.loadFromFile("bg2.jpg");
+	textureBackground_file.loadFromFile("bg0_2.png");
 	textureBackground_file.setRepeated(true);
 	textureBackground_file.setSmooth(true);
 	textureBackground.setTexture(textureBackground_file);
-	textureBackground.setTextureRect(sf::IntRect(0,0,1920,1080));
-	textureBackground.setScale(1080 / textureBackground_file.getSize().y, 1080 / textureBackground_file.getSize().y);
+	textureBackground.setScale(0.6,0.6);
+	textureBackground.setTextureRect(sf::IntRect(0,0,10000,10000));
 
 	//Add them to the vector
 	shapes.push_back(background);
