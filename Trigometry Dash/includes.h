e629c94585c1ffdf4fdb5bbae5806ddd395944b9
xzarfs <59253058+xzarfs@users.noreply.github.com>
2021-11-19 10:08:51 +0200

    Delete Trigometry Dash directory

--- Trigometry Dash/includes.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <iostream>
-#include <cstdlib>
-#include <chrono>
-#include <Windows.h>
-#include <ctime>
-#include <SFML/Graphics/Color.hpp>
